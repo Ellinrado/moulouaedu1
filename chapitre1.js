6e0bf0ddca269a9669bb524ab9d13ce33a8a36ae
Ellinrado <mouloua.el.mahdi@gmail.com>
2026-07-15 04:30:37 +0100

    chapitre1.js

---
+++ chapitre1.js
@@ -0,0 +1,63 @@
+const questionsChapitre1 = [
+
+{
+q:"Quel est le résultat de 4,5 + 2,3 ?",
+options:["6,8","6,2","7,8","5,8"],
+correct:0
+},
+
+{
+q:"Quel est le résultat de 9,4 − 2,8 ?",
+options:["6,6","7,2","5,6","6,2"],
+correct:0
+},
+
+{
+q:"Quel est le résultat de 3,5 × 2 ?",
+options:["5,5","7","6","8"],
+correct:1
+},
+
+{
+q:"Quel est le résultat de 8,4 ÷ 2 ?",
+options:["4,2","4","3,2","5,2"],
+correct:0
+},
+
+{
+q:"0,5 + 0,25 =",
+options:["0,75","0,30","0,80","0,70"],
+correct:0
+},
+
+{
+q:"10 − 3,6 =",
+options:["7,4","6,4","7,6","6,6"],
+correct:0
+},
+
+{
+q:"2,5 × 4 =",
+options:["6","8","10","12"],
+correct:2
+},
+
+{
+q:"7,2 ÷ 3 =",
+options:["2,4","2,1","3,4","2"],
+correct:0
+},
+
+{
+q:"12,6 + 3,4 =",
+options:["15","16","16,2","14"],
+correct:1
+},
+
+{
+q:"15,8 − 5,8 =",
+options:["9","10","11","8"],
+correct:1
+}
+
+];
