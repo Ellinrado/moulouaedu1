78c8eca2fb4e923fdd3623681d3652f9e2aa09cb
Ellinrado <mouloua.el.mahdi@gmail.com>
2026-07-16 01:50:21 +0100

    acquis1.js

--- acquis1.js
+++ acquis1.js
@@ -1,1 +1,93 @@
+const questionsAcquis1 = [
 
+{
+q:"Quel est le résultat de 4 + 5 ?",
+options:["7","8","9","10"],
+correct:2
+},
+
+{
+q:"Quel est le résultat de 12 − 7 ?",
+options:["4","5","6","7"],
+correct:1
+},
+
+{
+q:"Quel est le résultat de 6 × 8 ?",
+options:["42","46","48","54"],
+correct:2
+},
+
+{
+q:"Quel est le résultat de 45 ÷ 5 ?",
+options:["8","9","10","11"],
+correct:1
+},
+
+{
+q:"Dans le calcul 3 + 4 × 2, on effectue d'abord :",
+options:[
+"3 + 4",
+"4 × 2",
+"3 × 2",
+"Tout en même temps"
+],
+correct:1
+},
+
+{
+q:"Le nombre 7,35 est :",
+options:[
+"Un entier",
+"Un nombre décimal",
+"Une fraction",
+"Un nombre relatif"
+],
+correct:1
+},
+
+{
+q:"Quel est le résultat de 3,5 + 2,4 ?",
+options:[
+"5,9",
+"5,7",
+"6,1",
+"6"
+],
+correct:0
+},
+
+{
+q:"Le résultat de 9 × 0 est :",
+options:[
+"0",
+"1",
+"9",
+"Impossible"
+],
+correct:0
+},
+
+{
+q:"Quel est le résultat de 10,2 − 4 ?",
+options:[
+"5,8",
+"6,2",
+"6,4",
+"7,2"
+],
+correct:1
+},
+
+{
+q:"Quel est le résultat de 5 × (8 − 6) ?",
+options:[
+"8",
+"10",
+"12",
+"15"
+],
+correct:1
+}
+
+];
